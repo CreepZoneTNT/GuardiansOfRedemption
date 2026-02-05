3fdb977f7e2a92e2a148a6b92e7304da9ec41948
Jake Hatton <39422550+CreepZoneTNT@users.noreply.github.com>
2026-02-05 15:58:44 -0500

    Add KeeperGauntlet class with default properties
    No way to test it until I get home, but hopefully it should just work

--- Items/Weapons/Gauntlets/KeeperGauntlet.cs
+++ Items/Weapons/Gauntlets/KeeperGauntlet.cs
@@ -1,1 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
 
+namespace GuardiansOfRedemption.Items.Weapons.Gauntlets
+{
+	public class KeeperGauntlet : OrchidModGuardianGauntlet
+	{
+		public override void SafeSetDefaults()
+		{
+			Item.width = 44;
+			Item.height = 50;
+			Item.knockBack = 4f;
+			Item.damage = 120;
+			Item.value = Item.sellPrice(0, 1);
+			Item.rare = ItemRarityID.Blue;
+			Item.useTime = 20;
+			StrikeVelocity = 16f;
+			ParryDuration = 60;
+			PunchSpeed = 1.3f;
+		}
+
+		public override Color GetColor(bool offHand)
+		{
+			return new Color(117, 51, 66);
+		}
+	}
+}
