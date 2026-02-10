caa9c858a57a31da21c606fa3fe9920ebc6b1471
Gilded-Wyvern <casparftwaddington@gmail.com>
2026-02-10 21:51:27 +1300

    Created CS for Heavy Guard Chestplate

---
+++ Items/Armor/HeavyGuard/HeavyGuardChestplate.cs
@@ -0,0 +1,42 @@
+using GuardiansOfRedemption.Buffs;
+using OrchidMod;
+using OrchidMod.Content.Guardian;
+using Redemption;
+using Redemption.BaseExtension;
+using Redemption.Globals;
+using Redemption.Rarities;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+
+namespace GuardiansOfRedemption.Items.Armor.HeavyGuard
+{
+	[AutoloadEquip(EquipType.Body)]
+	public class GuardianBambooChest : HeavyGuardChestplate
+	{
+		public override void SafeSetDefaults()
+		{
+			Item.width = 30;
+			Item.height = 22;
+			Item.value = Item.sellPrice(0, 3, 0, 0);
+			Item.rare = ItemRarityID.Orange;
+			Item.defense = 7;
+		}
+
+		public override void UpdateEquip(Player player)
+		{
+			OrchidGuardian modPlayer = player.GetModPlayer<OrchidGuardian>();
+			modPlayer.GuardianGuardMax++;
+		}
+
+		public override void AddRecipes()
+		{
+			var recipe = CreateRecipe();
+			recipe.AddIngredient<Archcloth>, 3);
+      recipe.AddIngredient<GraveSteelAlloy>, 25);
+			recipe.AddTile(TileID.Sawmill);
+			recipe.Register();
+		}
+	}
+}
