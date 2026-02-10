ec7c44864ddc6b847cdbc4a8a2aa67b045e358a7
Gilded-Wyvern <casparftwaddington@gmail.com>
2026-02-10 21:58:01 +1300

    Update HeavyGuardChestplate.cs

--- Items/Armor/HeavyGuard/HeavyGuardChestplate.cs
+++ Items/Armor/HeavyGuard/HeavyGuardChestplate.cs
@@ -13,7 +13,7 @@ using Terraria.ModLoader.Config;
 namespace GuardiansOfRedemption.Items.Armor.HeavyGuard
 {
 	[AutoloadEquip(EquipType.Body)]
-	public class GuardianBambooChest : HeavyGuardChestplate
+	public class GuardianHeavyGuardChest : HeavyGuardChestplate
 	{
 		public override void SafeSetDefaults()
 		{
