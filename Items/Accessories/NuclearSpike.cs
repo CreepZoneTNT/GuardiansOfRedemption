db69d0b567e5b9fad8e1bd79ad23b6b14c840b58
Gilded-Wyvern <casparftwaddington@gmail.com>
2026-02-12 00:06:12 +1300

    Create NuclearSpike.cs

---
+++ Items/Accessories/NuclearSpike.cs
@@ -0,0 +1,41 @@
+using Terraria;
+using Terraria.ID;
+using OrchidMod;
+using OrchidMod.Content.Guardian;
+using Redemption;
+using Redemption.BaseExtension;
+using Redemption.Globals;
+using Redemption.Rarities;
+
+namespace GuardianOfRedemption.Items.Accessories
+{
+	public class NuclearSpike : OrchidModGuardianEquipable
+	{
+		public override void SafeSetDefaults()
+		{
+			Item.width = 30;
+			Item.height = 30;
+			Item.value = Item.sellPrice(0, 20, 0, 0);
+			Item.rare = ModContent.RarityType<TurquoiseRarity>();
+			Item.accessory = true;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			MoRGuardianPlayer modPlayer = player.GetModPlayer<MoRGuardianPlayer>();
+			if (modPlayer.GuardianSpikeDamage < 2.5f)
+				modPlayer.GuardianSpikeDamage = 2.5f;
+			modPlayer.GuardianSpikeNuclear = true;
+			modPlayer.GuardianSharpRebuttalBlock = true;
+			player.GetCritChance<GuardianDamageClass>() += 10;
+		}
+
+		public override void AddRecipes()
+		{
+			recipe.AddIngredient<TempleSpike>();
+			recipe.AddIngredient<Plutonium>(), 8;
+			recipe.AddTile(TileID.TinkerersWorkbench);
+			recipe.Register();
+		}
+	}
+}
